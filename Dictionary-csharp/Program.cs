4e0c4682b29469f7a2fa099c231e406993f0158d
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-30 11:20:40 -0300

    Adicionar arquivos de projeto.

---
+++ Dictionary-csharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmial.com";
+            cookies["phone"] = "99712234";
+            cookies["phone"] = "83737388";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("The is no 'email' key");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("All Cookies: ");
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+
+
+        }
+    }
+}
